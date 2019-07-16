dc7c2fb8c5631b9101a2118bfb845d6437c92307
George Kaye <georgejkaye@gmail.com>
2019-07-16 13:22:46 +0100

    Added sorting to fyp

--- pages/fyp/src/galleryFunctions.js
+++ pages/fyp/src/galleryFunctions.js
@@ -4,23 +4,50 @@
  * @author George Kaye
  */
 
-var terms;
+var terms = [];
+var completeTerms = [];
+
+/* [[cat1, [cat1_0, ... , cat1_n]], ... , [catn, [catn_0, ... , catn_n]]] */
+var sortedCategories = [];
+
 var cys;
 var ctx;
 var currentTermNo = 0;
-var termString = "";
 var totalNumber = 0;
 
 var n = 0;
 var k = 0;
-var cross = 0;
-var abs = 0;
-var apps = 0;
-var vars = 0;
-var betas = 0;
+var cross = -1;
+var abs = -1;
+var apps = -1;
+var vars = -1;
+var betas = -1;
 var fragment = "";
 
+/* Sort constants */
+const DEFAULT = 0;
+const CROSSINGS_HIGH_LOW = 1;
+const CROSSINGS_LOW_HIGH = 2;
+const BETA_HIGH_LOW = 3;
+const BETA_LOW_HIGH = 4;
+
+/* Functions for properties */
+const defaultProperty = x => x.id;
+const redexesProperty = x => x.betaRedexes();
+const crossingsProperty = x => x.crossings();
+
+/* Text for properties */
+const redexesPlural = "&beta;-redexes";
+const redexesSingle = "&beta;-redex";
+const crossingsPlural = "crossings";
+const crossingsSingle = "crossing";
+
+var propertyFunction;
+var propertySingle;
+var propertyPlural;
+
 var lastAction = 0;
+var lastSortMode = DEFAULT;
 
 /**
  * Change the text of an element with a given id.
@@ -76,8 +103,7 @@ function setStyleSpan(className, style){
                 elems2[k].setAttribute("style", style);
             }
         }
-    }
-    
+    } 
 }
 
 /**
@@ -112,8 +138,7 @@ function prettyString(array){
 /**
  * Action to perform when a generate button is performed.
  * @param {number} x - The identifier for the type of terms to generate.
- * @param {number} n - A previously specified n (optional).
- * @param {number} k - A previously specified k (optional).
+ * @param {boolean} prev - If this is not a new generation of terms (e.g. a filter or sort).
  */
 function generateButton(x, prev){
 
@@ -122,19 +147,14 @@ function generateButton(x, prev){
     if(!prev){
         n = parseInt(getText('n'));
         k = parseInt(getText('k'));
-        cross = parseInt(getText('crossings'));
-        apps = parseInt(getText('applications'));
-        abs = parseInt(getText('abstractions'));
-        vars = parseInt(getText('variables'));
-        betas = parseInt(getText('betas'));
         lastAction = x;
     }
 
-    var string = "";
-
     if(isNaN(n)){
-        string = "Bad input";
+        changeText('error', "Parse error: bad value of n");
+        clearButton(false);
     } else {
+        changeText('error', "");
 
         if(isNaN(k)){
             k = 0;
@@ -142,7 +162,7 @@ function generateButton(x, prev){
 
         if(!prev){
             fragment = "";
-            terms = [];
+            completeTerms = [];
             cys = [];
             freeVariables = new LambdaEnvironment();
 
@@ -152,51 +172,60 @@ function generateButton(x, prev){
 
             switch(lastAction){
                 case 0:
-                    terms = generateTerms(n, k);
+                    completeTerms = generateTerms(n, k);
                     fragment = "pure";
                     break;
                 case 1:
-                    terms = generateLinearTerms(n, k);
+                    completeTerms = generateLinearTerms(n, k);
                     fragment = "linear";
                     break;
                 case 2:
-                    terms = generatePlanarTerms(n, k);
+                    completeTerms = generatePlanarTerms(n, k);
                     fragment = "planar";
                     break;
             }
 
-            totalNumber = terms.length;
+            for(var i = 0; i < completeTerms.length; i++){
+               completeTerms[i].id = i;
+            }
+
+            terms = completeTerms;
+            totalNumber = completeTerms.length;
+
         }
 
-        if(!isNaN(cross)){
-            terms = terms.filter(x => x.crossings() === cross);
+        filterTerms();
+        sortTerms(true);
+        drawGallery();
+        document.getElementById('sort-btn').disabled = false;
+    }
+
+}
+
+/**
+ * Draw a gallery of generated terms.
+ * @param {boolean} prev - If the terms have previously been generated.
+ */
+function drawGallery(prev){
+    
+    var filteredNumber = terms.length;
+    var termString = "";
+
+    var portraitCount = 0;
+
+    for(var i = 0; i < sortedCategories.length; i++){
+
+        if(sortedCategories[i][0] !== ""){
+            termString += getDiv("", "category-" + i, "", "", getH("","", 3, "", "", sortedCategories[i][0]));
         }
 
-        if(!isNaN(apps)){
-            terms = terms.filter(x => x.applications() === apps);
-        }
-        
-        if(!isNaN(abs)){
-            terms = terms.filter(x => x.abstractions() === abs);
-        }
-        
-        if(!isNaN(vars)){
-            terms = terms.filter(x => x.crossings() === vars);
-        }
+        for(var j = 0; j < sortedCategories[i][1].length; j++){
 
-        if(!isNaN(betas)){
-            terms = terms.filter(x => x.betaRedexes() === betas);
-        }
+            var workingTerm = sortedCategories[i][1][j]
 
-        var filteredNumber = terms.length;
+            workingTerm.generatePrettyVariableNames(freeVariables);
 
-        termString = "";
-
-        for(i = 0; i < terms.length; i++){
-
-            terms[i].generatePrettyVariableNames(freeVariables);
-
-            var x = terms[i].prettyPrintLabels(freeVariables).length;
+            var x = workingTerm.prettyPrintLabels(freeVariables).length;
             var size = 200;
 
             while(x > 20){
@@ -207,72 +236,60 @@ function generateButton(x, prev){
             var termName = "";
 
             if(document.getElementById('de-bruijn').checked){
-                termName = terms[i].prettyPrint();
+                termName = workingTerm.prettyPrint();
             } else {
-                termName = printTermHTML(terms[i]);
+                termName = printTermHTML(workingTerm);
             }
 
-            var caption = getP("caption", "portrait-caption-" + i, "font-size:" + size + "%", "", termName + "<br>" + terms[i].crossings() + " crossings");
+            var caption = getSpan("caption", "portrait-caption-" + portraitCount, "font-size:" + size + "%", "", termName) + getSpan("subcaption", "portrait-subcaption-" + portraitCount, "", "", "<br>&#x2a09; " + workingTerm.crossings() + "&emsp;&beta; " + workingTerm.betaRedexes());
 
             if(document.getElementById("draw").checked){
-                termString += getDiv('w3-container frame', 'frame' + i, "", "viewPortrait('church-room', terms[" + i + "], false);", 
-                            getDiv("w3-container inner-frame", "", "", "", getDiv("w3-container portrait", "portrait" + i, "", "", "")) + "<br>" + 
+                termString += getDiv('w3-container frame', 'frame' + portraitCount, "", "viewPortrait('church-room', sortedCategories[" + i + "][1][" + j + "], false);", 
+                            getDiv("w3-container inner-frame", "", "", "", getDiv("w3-container portrait", "portrait" + portraitCount, "", "", "")) + "<br>" + 
                                 caption);            
- 
+
             } else {
-                termString += getDiv('w3-container frame empty', 'frame ' + i, "", "viewPortrait('church-room', terms[" + i + "], false);", caption);
+                termString += getDiv('w3-container frame empty', 'frame ' + portraitCount, "", "viewPortrait('church-room', sortedCategories[" + i + "][1][" + j + "], false);", caption);
             }
+
+            portraitCount++;
         }
-
-        changeText('church-room', termString);
-
-        var numString = "There ";
-        
-        if(totalNumber === 1){
-            numString += "is 1 " + fragment + " term";
-        } else {
-            numString += "are " + totalNumber + " " + fragment + " terms"; 
-        }
-
-        numString += " for n = " + n + " and k = " + k + "<br>" +
-                        filteredNumber + "/" + totalNumber + " term";
-
-        if(terms.length !== 1){
-            numString += "s";
-        }
-
-        var percentage = 0;
-
-        if(totalNumber != 0){
-            percentage = (filteredNumber / totalNumber) * 100;
-            changeText('help', 'Click on a term to learn more about it. ' + getButton("clear-btn", "clearButton()", "Clear all", false));
-        }
-
-        changeText('number-of-terms', numString + " match the filtering criteria: "  + percentage.toFixed(2) + "%");
-
-        ctx = new LambdaEnvironment();
-
-        for(var i = 0; i < k; i++){
-            ctx.pushTerm("f" + i, lambda + "f" + i + ".");
-        }
-
-        drawGallery(false, terms, ctx);
-        scrollToElement('number-of-terms');
-
     }
 
-}
+    changeText('church-room', termString);
 
-/**
- * Draw a gallery of generated terms.
- * @param {boolean} cache - If the terms have previously been generated.
- * @param {terms}   terms - The terms in the gallery.
- * @param {ctx}     ctx   - The context of the gallery.
- */
-function drawGallery(cache, terms, ctx){
+    var numString = "There ";
     
+    if(totalNumber === 1){
+        numString += "is 1 " + fragment + " term";
+    } else {
+        numString += "are " + totalNumber + " " + fragment + " terms"; 
+    }
+
+    numString += " for n = " + n + " and k = " + k + "<br>" +
+                    filteredNumber + "/" + totalNumber + " term";
+
+    if(terms.length !== 1){
+        numString += "s";
+    }
+
+    var percentage = 0;
+
+    if(totalNumber != 0){
+        percentage = (filteredNumber / totalNumber) * 100;
+        changeText('help', 'Click on a term to learn more about it. ' + getButton("clear-btn", "clearButton(true)", "Clear all", false));
+    }
+
+    changeText('number-of-terms', numString + " match the filtering criteria: "  + percentage.toFixed(2) + "%");
+
+    ctx = new LambdaEnvironment();
+
+    for(var i = 0; i < k; i++){
+        ctx.pushTerm("f" + i, lambda + "f" + i + ".");
+    }
+
     if(document.getElementById("draw").checked){
-        if(cache){
+        if(prev){
             for(var i = 0; i < terms.length; i++){
                 drawMap("portrait" + i, terms[i], ctx, false, false, false);
             }
@@ -282,20 +299,29 @@ function drawGallery(cache, terms, ctx){
             cys[i] = drawMap("portrait" + i, terms[i], ctx, false, false, false);
         }
     }
+    
+    scrollToElement('filtering-options');
+
 }
 
 var a = 0;
 
 /**
  * Function to execute when the clear button is pressed.
+ * @param {boolean} nums - Whether to clear the number boxes.
  */
-function clearButton(){
+function clearButton(nums){
     changeText('church-room', "");
     changeText('number-of-terms', "");
     changeText('help', "");
     changeText('normalisation-studio', "");
-    changeValueClass('number-box', "");
+    
+    if(nums){
+        changeValueClass('number-box', "");
+    }
+
     scrollToElement();
+    document.getElementById('sort-btn').disabled = true;
 }
 
 /**
@@ -306,4 +332,171 @@ function backButton(){
     generateButton(lastAction, true);
     reduced = false;
     scrollToElement('number-of-terms');
+}
+
+/**
+ * Function to execute when the filter and sort button is pressed.
+ */
+function filterAndSortButton(){
+    
+    var filterChanged = filterTerms()
+    var sortChanged = sortTerms(filterChanged);
+    
+    if(filterChanged || sortChanged){
+        drawGallery();
+    }
+}
+
+/**
+ * Filter the current gallery based on the criteria specified by the user.
+ * @return {boolean} If the list of terms or the order has been changed.
+ */
+function filterTerms(){
+
+    var changed = false;
+    var newCross = parseIntOrEmpty(getText('crossings'));
+    var newApps = parseIntOrEmpty(getText('applications'));
+    var newAbs = parseIntOrEmpty(getText('abstractions'));
+    var newVars = parseIntOrEmpty(getText('variables'));
+    var newBetas = parseIntOrEmpty(getText('betas'));
+
+    if(newCross !== cross || newApps !== apps || newAbs !== abs || newVars !== vars || newBetas !== betas){
+        changed = true;
+        terms = completeTerms;
+        cross = newCross;
+        apps = newApps;
+        abs = newAbs;
+        vars = newVars;
+        betas = newBetas;
+    }
+
+    if(cross !== -1){
+        terms = completeTerms.filter(x => x.crossings() === cross);
+    }
+
+    if(apps !== -1){
+        terms = completeTerms.filter(x => x.applications() === apps);
+    }
+    
+    if(abs !== -1){
+        terms = completeTerms.filter(x => x.abstractions() === abs);
+    }
+    
+    if(vars !== -1){
+        terms = completeTerms.filter(x => x.crossings() === vars);
+    }
+
+    if(betas !== -1){
+        terms = completeTerms.filter(x => x.betaRedexes() === betas);
+    }
+
+    return changed;
+}
+
+/**
+ * Filter the current gallery based on the method specified by the user.
+ * @param {boolean} filter - If the filter has changed and so the categories need to be recalculated.
+ * @return {boolean} If the list of terms or the order has been changed.
+ */
+function sortTerms(filter){
+
+    var changed = false;
+
+    var mode = document.getElementById("sort").selectedIndex;
+
+    if(mode !== lastSortMode){
+        
+        changed = true;
+        lastSortMode = mode;
+
+        switch(mode){
+            case DEFAULT:
+                order = true;
+                propertyFunction = defaultProperty;
+                break;
+            case CROSSINGS_HIGH_LOW:
+                order = false;
+                propertyFunction = crossingsProperty;
+                propertyNameSingle = crossingsSingle;
+                propertyNamePlural = crossingsPlural;
+                break;
+            case CROSSINGS_LOW_HIGH:
+                order = true;
+                propertyFunction = crossingsProperty;
+                propertyNameSingle = crossingsSingle;
+                propertyNamePlural = crossingsPlural;
+                break;
+            case BETA_HIGH_LOW:
+                order = false;
+                propertyFunction = redexesProperty;
+                propertyNameSingle = redexesSingle;
+                propertyNamePlural = redexesPlural;
+                break;
+            case BETA_LOW_HIGH:
+                order = true;
+                propertyFunction = redexesProperty;
+                propertyNameSingle = redexesSingle;
+                propertyNamePlural = redexesPlural;
+                break;
+        }
+
+        terms = quickSortTerms(terms, propertyFunction, order);
+    }
+
+    if(mode !== DEFAULT){
+
+        var c = 0;
+        var currentProperty = propertyFunction(terms[0]);
+
+        var propertyName = currentProperty === 1 ? propertyNameSingle : propertyNamePlural;
+        
+        sortedCategories =  [];
+        sortedCategories[0] = [currentProperty + " " + propertyName, [terms[0]]];
+
+        for(var i = 1; i < terms.length; i++){
+            
+            var newProperty = propertyFunction(terms[i]);
+
+            if(currentProperty !== newProperty){
+                c++;
+                propertyName = newProperty === 1 ? propertyNameSingle : propertyNamePlural;
+                sortedCategories[c] = [propertyFunction(terms[i]) + " " + propertyName, [terms[i]]];
+                currentProperty = newProperty;
+            } else {
+                smartPush(sortedCategories[c][1], terms[i]);
+            }
+        }
+    } else {
+        sortedCategories[0] = ["", terms];
+    }
+
+    return changed;
+}
+
+/**
+ * Sort terms by a certain property that they have.
+ * @param {Object[]} termList - The list of terms to sort.
+ * @param {function} propertyFunction - The function to get the property to sort by.
+ * @param {boolean} order - If to sort them from high to low (false) or low to high (true).
+ * @return {Object[]} The sorted list of terms.
+ */
+function quickSortTerms(termList, propertyFunction, order){
+
+    if (termList.length <= 1){
+        return termList;
+    }
+
+    var pivot = termList[0];
+    termList = termList.slice(1);
+
+    var highers = quickSortTerms(termList.filter(x => propertyFunction(x) >= propertyFunction(pivot)), propertyFunction, order);
+    var lowers = quickSortTerms(termList.filter(x => propertyFunction(x) < propertyFunction(pivot)), propertyFunction, order);
+
+    if (order){
+        return lowers.concat([pivot].concat(highers));
+    }
+
+    var temp = [pivot].concat(lowers);
+    return highers.concat(temp);
+ 
 }
