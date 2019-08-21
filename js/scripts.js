be74c64af06cd6c7c671987ac3a3b6f20e31cb1a
George Kaye <georgejkaye@gmail.com>
2019-08-21 12:21:08 +0100

    Made link highlighting work with hacky jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
 
+	$(function(e){
+		if(document.location.pathname == '/') {
+			var hash = window.location.hash.substr(1);
+			console.log(hash);
+			$('#' + hash + '-link').addClass('active');
+		}
+	});
+
 	$('h2,h3').not('.keep-id').each(function(){
 		var hyphenated = $(this).text().toLowerCase().replace(/[^a-z0-9\s-]/gi, '').replace(/\s/g, '-');
 		$(this).attr('id', hyphenated);
 	});
 
-	$(".home-item").on('click', function (e) {
+	/*$(".home-item").on('click', function (e) {
 		console.log("work");
 		$(".active").removeClass('active');
 		$(this).addClass('active');
-	}); 
+	}); */
 
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -21,9 +29,13 @@ $(document).ready(function(){
 	        'scrollTop': $target.offset().top
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
-	    });
+		});
+		
+		if(document.location.pathname == '/') {
+			$('.active').removeClass('active');
+			console.log("target");
+			$(target + '-link').addClass('active');
+		}
 	});
-
-	
     
 });
