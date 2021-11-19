15413913eb3af7e0184cb403251c80255c0cdec7
George Kaye <georgejkaye@gmail.com>
2021-11-19 00:22:46 +0000

    Continued making the website

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const yaml = require("js-yaml");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = config => {
     config.addPassthroughCopy("images")
     config.addPassthroughCopy("files")
     config.addPassthroughCopy("scripts")
     config.addDataExtension("yml", contents => yaml.load(contents))
+    config.addPlugin(syntaxHighlight)
     config.setBrowserSyncConfig({
         files: './build/css/**/*.css'
     });
