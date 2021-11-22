83c1f5e1a6c05e7d1de3e5da64f52701586e692c
George Kaye <georgejkaye@gmail.com>
2021-11-22 23:03:16 +0000

    Transferring over pictures and stations pages

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,25 @@ module.exports = config => {
     config.addPassthroughCopy("images")
     config.addPassthroughCopy("files")
     config.addPassthroughCopy("scripts")
+    config.addLiquidFilter("join", function (arr) {
+        var str = ""
+        for (const s of arr) {
+            str += s
+        }
+        return str
+    });
+    config.addLiquidFilter("first", function (arr) {
+        return arr[0]
+    });
+    config.addLiquidFilter("last", function (arr) {
+        return arr[arr.length - 1]
+    });
+    config.addLiquidFilter("slice", function (string, i, j) {
+        return string.slice(i, j)
+    });
+    config.addLiquidFilter("firstChar", function (string) {
+        return string.charAt(0).toLowerCase()
+    })
     config.addDataExtension("yml", contents => yaml.load(contents))
     config.addPlugin(syntaxHighlight)
     config.setBrowserSyncConfig({
