d4009fbcaa1be21ee648dd6a99696aad41ddd715
George Kaye <georgejkaye@gmail.com>
2021-11-30 00:27:01 +0000

    Got a nice tag system working

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
-const yaml = require("js-yaml");
+const yaml = require("js-yaml")
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
-const moment = require("moment");
+const moment = require("moment")
+const lodashChunk = require("lodash.chunk")
 
 module.exports = config => {
     config.addPassthroughCopy("images")
@@ -12,33 +13,63 @@ module.exports = config => {
             str += s
         }
         return str
-    });
+    })
     config.addLiquidFilter("first", function (arr) {
         return arr[0]
-    });
+    })
     config.addLiquidFilter("last", function (arr) {
         return arr[arr.length - 1]
-    });
+    })
     config.addLiquidFilter("slice", function (string, i, j) {
         return string.slice(i, j)
-    });
+    })
     config.addLiquidFilter("firstChar", function (string) {
         return string.charAt(0).toLowerCase()
     })
     config.addLiquidFilter("increment", function (x) {
         return x + 1
     })
-    // Inside the function you export...
+    config.addLiquidFilter("reverse", function (arr) {
+        return arr.reverse()
+    })
     config.addLiquidFilter("prettyDate", (date) => {
-        const utc = date.toUTCString();
-        return moment.utc(utc).format("DD MMMM YYYY");
-    });
+        const utc = date.toUTCString()
+        return moment.utc(utc).format("DD MMMM YYYY")
+    })
+    config.addCollection("doublePagination", function (collection) {
+        let tagSet = new Set()
+        collection.getAllSorted().map(function (item) {
+            if ("tags" in item.data) {
+                let tags = item.data.tags
+                for (let tag of tags) {
+                    tagSet.add(tag)
+                }
+            }
+        })
+        let paginationSize = 3
+        let tagMap = []
+        let tagArray = [...tagSet]
+        for (let tagName of tagArray) {
+            let tagItems = collection.getFilteredByTag(tagName)
+            tagItems.reverse()
+            let pagedItems = lodashChunk(tagItems, paginationSize)
+            for (let pageNumber = 0, max = pagedItems.length; pageNumber < max; pageNumber++) {
+                tagMap.push({
+                    tagName: tagName,
+                    pageNumber: pageNumber,
+                    pageData: pagedItems[pageNumber],
+                    pages: pagedItems.length
+                })
+            }
+        }
+        return tagMap
+    })
     config.addDataExtension("yml", contents => yaml.load(contents))
     config.addPlugin(syntaxHighlight)
-    config.setDataDeepMerge(true);
+    config.setDataDeepMerge(true)
     config.setBrowserSyncConfig({
         files: './build/css/**/*.css'
-    });
+    })
     return {
         dir: {
             input: "src",
