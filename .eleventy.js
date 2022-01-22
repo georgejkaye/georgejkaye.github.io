e2b5c53e61eafdfb3fdf31bf593d111acd927039
George Kaye <georgejkaye@gmail.com>
2022-01-22 23:01:48 +0000

    Fixed the breaking changes introduced by eleventy 1.0.0

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const moment = require("moment")
 const lodashChunk = require("lodash.chunk")
 
+
 module.exports = config => {
     config.addPassthroughCopy("images")
     config.addPassthroughCopy("files")
