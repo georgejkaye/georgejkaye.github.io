bb16a8fd90832b9dddf30af5467ea15a95a84538
George Kaye <georgejkaye@gmail.com>
2022-08-12 22:16:35 +0100

    Improve how talks, papers are rendered

--- src/includes/shortcodes/home.js
+++ src/includes/shortcodes/home.js
@@ -3,89 +3,92 @@ const moment = require("moment")
 
 const navLink = `<div class="nav-link"><a href={{ link.target }}>{{ link.title }}</a></div>`
 
-const button = (target, title, text) =>
-    outdent`<span class="link-button">[<a href="${target}" title="${title}">${text}</a>]</span>`
+const button = (array, target, title, text) =>
+    array.push(
+        outdent`<span class="link-button"><a href="${target}" title="${title}">${text}</a></span>`
+    )
 
-const buttonOrBlank = (key, link, title, text) => (
-    key
-        ? button(
+const buttonOrBlank = (array, key, link, title, text) => {
+    if (key) {
+        button(
+            array,
             link,
             title,
             text
         )
-        : ""
-)
+    }
+}
 
 const makeButtons = (buttons) =>
-    buttons.reduce((acc, cur, i) => `${acc}${cur}${i == buttons.length - 1 ? "" : " "}`, "")
+    outdent`
+        <div>
+            ${buttons.reduce((acc, cur, i) => `${acc}${cur}${i == buttons.length - 1 ? "" : " • "}`, "")}
+        </div>
+    `
 
 const paper = (paper, people) => {
-    let titleText = `<span class="paper-title">${paper.title}</span>`
-    let buttons = [
-        buttonOrBlank(
-            paper.page,
-            paper.page,
-            `Page for '${paper.page}`,
-            "page"
-        ),
-        buttonOrBlank(
-            paper.arxiv,
-            `/files/papers/${paper.file}.pdf`,
-            `PDF version of '${paper.title}'`,
-            `pdf`
-        ),
-        buttonOrBlank(
-            paper.arxiv,
-            `https://arxiv.org/abs/${paper.arxiv}`,
-            `Arxiv page for '${paper.title}'`,
-            `arxiv`
-        ),
-        button(
-            `/files/papers/${paper.file}.bib.txt`,
-            `Bibtex entry for '${paper.title}'`,
-            `bibtex`
-        ),
-        outdent`
-            [<span 
-                title="Toggle the abstract for '${paper.title}'" 
-                id="${paper.file}"
-                class="toggle-abstract"
-            >abstract</span>]
-        `
-    ]
-    let buttonText = makeButtons(buttons)
+    let titleText = `<span class="paper-title"> ${paper.title}</span>`
+    let buttons = []
+    buttonOrBlank(
+        buttons,
+        paper.page,
+        paper.page,
+        `Page for '${paper.page}`,
+        "page"
+    )
+    buttonOrBlank(
+        buttons,
+        paper.arxiv,
+        `/files/papers/${paper.file}.pdf`,
+        `PDF version of '${paper.title}'`,
+        `pdf`
+    )
+    buttonOrBlank(
+        buttons,
+        paper.arxiv,
+        `https://arxiv.org/abs/${paper.arxiv}`,
+        `Arxiv page for '${paper.title}'`,
+        `arxiv`
+    )
+    button(
+        buttons,
+        `/files/papers/${paper.file}.bib.txt`,
+        `Bibtex entry for '${paper.title}'`,
+        `bibtex`
+    )
+    let buttonText = outdent`
+        <div class="talk-buttons">
+            ${makeButtons(buttons)}
+        </div>
+    `
     let detailsText = outdent`
         <div class="paper-details">
             ${paper.details}
         </div>`
     let authorText =
-        paper.authors.reduce(
-            (acc, cur, i) => {
-                let person = people[cur.name]
-                let nameText =
-                    person.web
-                        ? `<a href="${person.web}"
-                            title="${person.name}${person.poss} personal webpage"
-                            >${person.name}</a>`
-                        : person.name
-                return outdent`
-                    ${acc}
-                    <span class="author${cur.name == "me" ? " author-me" : ""}">${cur.text ? `${cur.text} ` : ""}${nameText}</span>${i != paper.authors.length - 1 ? ", " : ""}
-                `
-            }
-            , "")
+        outdent`<div class="paper-authors">
+                ${paper.authors.reduce((acc, cur, i) => {
+            let person = people[cur.name]
+            let nameText =
+                person.web
+                    ? `<a href="${person.web}"
+                                title="${person.name}${person.poss} personal webpage"
+                                >${person.name}</a>`
+                    : person.name
+            return outdent`
+                ${acc}<span class="author${cur.name == "me" ? " author-me" : ""}">${cur.text ? `${cur.text} ` : ""}${nameText}</span>${i != paper.authors.length - 1 ? ", " : ""}
+            `
+        }
+            , "")}</div>`
     let abstractText = outdent`
-        <div id="${paper.file}-abstract" class="paper-abstract">
+        <div id="${paper.file}-abstract" class="paper-abstract" >
             ${paper.abstract}
         </div>
     `
     let paperReturn = outdent`
-        <div class="card">
-            ${titleText}
-            ${buttonText}
-            ${detailsText}
-            ${authorText}
-            ${abstractText}
+    <div class="card">
+        ${titleText}
+            ${authorText}${detailsText}${abstractText}${buttonText}
         </div>
     `
     return paperReturn
@@ -95,33 +98,37 @@ const talk = (talk) => {
     let date = talk.date.toUTCString()
     let prettyDate = moment.utc(date).format("MMMM DD, YYYY")
     let fileDate = moment.utc(date).format("YYYY-MM-DD")
-    let buttons = [
-        buttonOrBlank(
-            talk.links.slides,
-            `/files/slides/${fileDate}-${talk.key}-slides.pdf`,
-            `Slides for '${talk.title}`,
-            `slides`
-        ),
-        buttonOrBlank(
-            talk.links.abstract,
-            `/files/abstracts/${fileDate}-${talk.key}-abstract.pdf`,
-            `Extended abstract for ${talk.title}`,
-            `extended abstract`
-        ),
-        buttonOrBlank(
-            talk.links.video,
-            `${talk.links.video}`,
-            `Video of '${talk.title}'`,
-            `video`
-        ),
-        buttonOrBlank(
-            talk.links.github,
-            `https://github.com/georgejkaye?tab=repositories&q=${talk.key}`,
-            `Github repos for '${talk.title}`,
-            `github`
-        )
-    ]
-    let buttonText = makeButtons(buttons)
+    let buttons = []
+    buttonOrBlank(
+        buttons,
+        talk.links.slides,
+        `/files/slides/${fileDate}-${talk.key}-slides.pdf`,
+        `Slides for '${talk.title}`,
+        `slides`
+    )
+    buttonOrBlank(
+        buttons,
+        talk.links.abstract,
+        `/files/abstracts/${fileDate}-${talk.key}-abstract.pdf`,
+        `Extended abstract for ${talk.title}`,
+        `extended abstract`
+    )
+    buttonOrBlank(
+        buttons,
+        talk.links.video,
+        `${talk.links.video}`,
+        `Video of '${talk.title}'`,
+        `video`
+    )
+    buttonOrBlank(
+        buttons,
+        talk.links.github,
+        `https://github.com/georgejkaye?tab=repositories&q=${talk.key}`,
+        `Github repos for '${talk.title}`,
+        `github`
+    )
+    let buttonText = outdent`<div class="talk-buttons">
+        ${makeButtons(buttons)}</div>`
     return outdent`
         <div class="card">
             <div class="talk-title">${talk.title}</div>
@@ -132,8 +139,8 @@ const talk = (talk) => {
                 <span class="talk-date">
                     ${prettyDate}
                 </span>
-                ${buttonText}
             </div>
+            ${buttonText}
         </div>
     `
 }
@@ -164,20 +171,22 @@ const teaching = (teaching) => {
                 <span class="module-role">${teaching.role}</span>
                 <span class="module-institution">${teaching.institution}</span>
     `
-    let links = teaching.links
-        ? makeButtons(
+    let buttons = []
+    if (teaching.links) {
+        makeButtons(
             teaching.links.map((link) =>
                 button(
+                    buttons,
                     link.target,
                     link.title,
                     link.text
                 ))
         )
-        : ""
+    }
     return outdent`
         <div class="card">
         ${first}
-        ${links}
+        ${buttons}
         </div ></div >
     `
 }
