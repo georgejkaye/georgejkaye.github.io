e378ad776246a0966ff90218f986742598cf853b
georgejkaye <georgejkaye@users.noreply.github.com>
2023-04-12 19:24:55 +0000

    Automated deployment: Wed Apr 12 19:24:55 UTC 2023 53089179c07c29bff40512d9fb509279890e0907

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	// Add ids to each header, so we can use them as anchors
+	$('h2,h3').not('.keep-id').each(function () {
+		var hyphenated = $(this).text().toLowerCase().replace(/[^a-z0-9\s-]/gi, '').replace(/\s/g, '-');
+		$(this).attr('id', hyphenated);
+	});
+	// Show an abstract when you click on a paper
+	$('.toggle-abstract').click(function () {
+		var id = "#" + $(this).attr('id') + "-abstract"
+		$(id).css("display") === "block" ? $(id).hide() : $(id).show()
+	})
+});
