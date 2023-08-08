21abbfe1947ca139351c182c03761ec8656da96a
georgejkaye <georgejkaye@gmail.com>
2023-08-08 21:44:41 +0000

    Update main to output generated at 4d4daee

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+	let mobileNav = "#nav-mobile"
+	$(mobileNav).hide()
 	// Add ids to each header, so we can use them as anchors
 	$('h2,h3').not('.keep-id').each(function () {
 		var hyphenated = $(this).text().toLowerCase().replace(/[^a-z0-9\s-]/gi, '').replace(/\s/g, '-');
@@ -8,5 +10,9 @@ $(document).ready(function () {
 	$('.toggle-abstract').click(function () {
 		var id = "#" + $(this).attr('id') + "-abstract"
 		$(id).css("display") === "block" ? $(id).hide() : $(id).show()
+	})
+	// Show mobile nav when box is clicked
+	$('.nav-button').click(function () {
+		$(mobileNav).css("display") === "none" ? $(mobileNav).show() : $(mobileNav).hide()
 	})
 });
