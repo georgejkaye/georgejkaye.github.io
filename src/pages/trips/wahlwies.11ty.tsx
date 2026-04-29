31dda592a27b2a06ad61872e90a17e134fe4ddba
George Kaye <georgejkaye@gmail.com>
2026-04-29 23:34:20 +0100

    feat: continue to make wahwies page

--- src/pages/trips/wahlwies.11ty.tsx
+++ src/pages/trips/wahlwies.11ty.tsx
@@ -1,24 +1,62 @@
-interface ServiceCardProps {
+interface Service {
   id: string;
   operator: string;
   origin: string;
   dep: Date;
+  depHourOffset?: number
   destination: string;
   arr: Date;
+  arrHourOffset?: number
 }
 
-const ServiceCard = ({
-  id,
-  operator,
-  origin,
-  dep,
-  destination,
-  arr,
-}: ServiceCardProps) => (
-  <div style={{ display: "flex", flexDirection: "column" }}>
-    <div>{id}</div>
+const ServiceCard = ({ service }: { service: Service }) => {
+  const depHourGb = new Date(service.dep.getTime())
+  depHourGb.setHours(depHourGb.getHours() - (service.depHourOffset ?? 0))
+  const arrHourGb = new Date(service.arr.getTime())
+  arrHourGb.setHours(arrHourGb.getHours() - (service.arrHourOffset ?? 0))
+  const duration = arrHourGb.getTime() - depHourGb.getTime()
+  return (
+    <div className="flex flex-col gap-2 rounded-xl bg-pink-800 p-4">
+      <div className="flex flex-row items-center">
+        <div className="text-2xl w-20">{service.dep.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" })}</div>
+        <div className="text-xl font-bold">{service.origin}</div>
+      </div>
+      <div className="flex flex-row items-center">
+        <div className="text-2xl w-20">{service.arr.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" })}</div>
+        <div className="text-xl font-bold">{service.destination}</div>
+      </div>
+      <div className="flex flex-row gap-2">
+        <div>{Math.floor(duration / 3600000)}h {Math.floor(duration / 60000 % 60)}m</div>
+        <div>•</div>
+        <div>{service.operator}</div>
+        <div>•</div>
+        <div>{service.id}</div>
+      </div>
+    </div>
+  )
+}
+
+const ConnectionCard = ({ first, second }: { first: Service, second: Service }) => {
+  const connectionTime = second.dep.getTime() - first.arr.getTime()
+  return (
+    <div className="rounded-xl px-4 py-2 bg-blue-900">
+      {Math.floor(connectionTime / 3600000)} hour {Math.floor(connectionTime / 60000 % 60)} minute connection
+    </div>
+  )
+
+}
+
+const ServiceList = ({ services }: { services: Service[] }) => (
+  <div className="flex flex-col gap-4">
+    <h4>Itinerary</h4>
+    {services.map((service, i) => (
+      <>
+        <ServiceCard key={service.id} service={service} />
+        {i !== services.length - 1 && <ConnectionCard first={service} second={services[i + 1]} />}
+      </>
+    ))}
   </div>
-);
+)
 
 interface Data {
   permalink: string;
@@ -27,34 +65,39 @@ interface Data {
 export default ({ permalink }: Data) => {
   return (
     <div>
-      <h1>Wahlwies</h1>
       <h2>Thursday 02 July</h2>
       <h3>Birmingham to Strasbourg</h3>
-      <h4>Itinerary</h4>
-      <ServiceCard
-        id="VT07520"
-        operator="Avanti West Coast"
-        origin="Birmingham New Street"
-        dep={new Date(2026, 7, 2, 8, 17)}
-        destination="London Euston"
-        arr={new Date(2026, 7, 4, 9, 41)}
-      />
-      <ServiceCard
-        id="9022"
-        operator="Eurostar"
-        origin="London St Pancras International"
-        dep={new Date(2026, 7, 2, 11, 31)}
-        destination="Paris Nord"
-        arr={new Date(2026, 7, 2, 14, 49)}
-      />
-      <ServiceCard
-        id="9577"
-        operator="TGV INOUI"
-        origin="Paris Est"
-        dep={new Date(2026, 7, 2, 15, 55)}
-        destination="Paris Nord"
-        arr={new Date(2026, 7, 2, 17, 41)}
-      />
+      <ServiceList services={
+        [
+          {
+            id: "VT07520",
+            operator: "Avanti West Coast",
+            origin: "Birmingham New Street",
+            dep: new Date(2026, 6, 2, 8, 17),
+            destination: "London Euston",
+            arr: new Date(2026, 6, 2, 9, 41),
+          },
+          {
+            id: "9022",
+            operator: "Eurostar",
+            origin: "London St Pancras International",
+            dep: new Date(2026, 6, 2, 11, 31),
+            destination: "Paris Nord",
+            arr: new Date(2026, 6, 2, 14, 49),
+            arrHourOffset: 1
+          },
+          {
+            id: "9577",
+            operator: "TGV INOUI",
+            origin: "Paris Est",
+            depHourOffset: 1,
+            dep: new Date(2026, 6, 2, 15, 55),
+            destination: "Strasbourg",
+            arr: new Date(2026, 6, 2, 17, 41),
+            arrHourOffset: 1
+          }
+        ]
+      } />
     </div>
   );
 };
